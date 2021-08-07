1f4dbb78d23db9073676339981b8c15d0d2bae7d
NiveenALSmadi <niveensmadi33@gmail.com>
2021-08-07 23:25:33 +0300

    done

--- components/header.js
+++ components/header.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'  
-export default function Header(props){
+export default function CookieStandHeader(props){
     return(
         <header className="flex justify-between bg-green-500 text-black-100 p-3 items-center">
-        <h1 className="font-semibold text-2xl font-sans">Cookie Stand Admin</h1>
+        <h1 className="font-semibold text-2xl font-sans mx-9">Cookie Stand Admin</h1>
         <Link href="/overview">
         <button className=" text-xs font-sans">OverView</button>
         </Link>
